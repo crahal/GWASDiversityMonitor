1e50046d218bffef622fbe1a223495aca097cdb3
CRahal <charles.rahal@sociology.ox.ac.uk>
2019-09-06 00:25:21 +0100

    daily update

--- diversity_updater.sh
+++ diversity_updater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git pull .
 cd /media/charlie/raspi_usb/gwasdiversitymonitor/
+git pull origin master
 /home/charlie/berryconda3/bin/python3 generate_data.py
 git add .
 git commit -m "daily update"
