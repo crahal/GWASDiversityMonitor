f0d672f2450f7a87702ffb07aae00a1d983ddbe5
crahal <charlierahal@gmail.com>
2019-09-12 11:50:20 +0100

    daily update

--- diversity_updater.sh
+++ diversity_updater.sh
@@ -5,6 +5,7 @@ rm -rf .fuse*
 git add .
 git reset HEAD -- correspondance/
 git reset HEAD -- __pycache__/
+git reset HEAD -- gwasdiversitymonitor_app/__pycache__/
 git reset HEAD -- gwasdiversitymonitor_app/data/catalog/cached_files/*/*
 git reset HEAD -- *swp
 git commit -m "daily update"
